b54a4b4d650bd0eb4199d96ab502a674b463f988
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-23 13:30:18 +0000

    Deploy mgorshkov/queue to github.com/mgorshkov/queue.git:gh-pages

--- search/all_c.js
+++ search/all_c.js
@@ -6,6 +6,7 @@ var searchData=
   ['startqueuesession',['StartQueueSession',['../Message_8h.html#ac6606ebe91c8ac66a2c314c79f5ab013a60f5c2c8f11066fbc9f7e80204a1b5bd',1,'Message.h']]],
   ['startqueuesessionmessage',['StartQueueSessionMessage',['../structStartQueueSessionMessage.html',1,'StartQueueSessionMessage'],['../structStartQueueSessionMessage.html#a238b8e1554d0a9e4fe77d1f314e6bfc2',1,'StartQueueSessionMessage::StartQueueSessionMessage()']]],
   ['startqueuesessionmessageptr',['StartQueueSessionMessagePtr',['../Message_8h.html#acbd1e12a312dc1487ca6f51f1514b1d2',1,'Message.h']]],
+  ['startsession',['StartSession',['../Defines_8h.html#a2afce0a47a93eee73a314d53e4890153ab3cf27cc1224b4a4341c993566eb16f3',1,'Defines.h']]],
   ['sync',['Sync',['../Defines_8h.html#afa1e7d4f8cec163e0b89401341524112ad8e87c0927539672f54462c837be0b7f',1,'Defines.h']]],
   ['syncasyncmode',['SyncAsyncMode',['../Defines_8h.html#afa1e7d4f8cec163e0b89401341524112',1,'Defines.h']]]
 ];
