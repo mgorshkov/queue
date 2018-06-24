f5ef1df03413ffe37bcc69900a95890b53929d7b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-24 11:53:30 +0000

    Deploy mgorshkov/queue to github.com/mgorshkov/queue.git:gh-pages

--- search/all_2.js
+++ search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['client',['Client',['../classClient.html',1,'Client'],['../classClient.html#a777bda53e94d9fa1cbdfede30f0dba8b',1,'Client::Client()']]],
+  ['client',['Client',['../classClient.html',1,'Client'],['../classClient.html#ae8af4121edf190fb847b62ab4643db64',1,'Client::Client()']]],
   ['client_2ecpp',['Client.cpp',['../Client_8cpp.html',1,'']]],
   ['client_2eh',['Client.h',['../Client_8h.html',1,'']]],
   ['clientmain_2ecpp',['ClientMain.cpp',['../ClientMain_8cpp.html',1,'']]],
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['client',['Client',['../classClient.html#a777bda53e94d9fa1cbdfede30f0dba8b',1,'Client']]]
+  ['client',['Client',['../classClient.html#ae8af4121edf190fb847b62ab4643db64',1,'Client']]]
 ];
